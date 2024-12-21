68cbe39818fb0c132e6345a9993c4031f147ac92
pubalibxo <pubali2003@gmail.com>
2024-12-21 20:28:29 +0530

    DevTrack: Files: d4.py.  | Changes in: d4.py

--- d4.py
+++ d4.py
@@ -3,5 +3,22 @@
 #     if i%2==0:
 #         print(i)
 
-if i== i*i+1/2:
-    print(i)
+# Print the multiplication table of 5 using a for loop.
+# n=5
+# for i in range (1,11):
+#     print(n*i)
+# Print all numbers divisible by 3 between 1 and 30.
+# for i in range (31):
+#     if i%3==0:
+#         print(i)
+# Count the number of even numbers between 1 and 50.
+# for i in range (51):
+#     if i%2==0:
+#         print(i)
+# Create a program that prints the factorial of a number using a for loop.
+n=int(input("enter the number for which you want the factorial:"))
+fact=1
+for i in range(1, n+1):
+    fact*=i
+
+print(fact)
